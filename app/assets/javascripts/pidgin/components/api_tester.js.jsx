9df8b74992a1ee698fce4e19073c193c347dfeb5
hokuma <okuma@hokuma.net>
2016-11-24 15:35:52 +0900

    :sparkles: api call

--- app/assets/javascripts/pidgin/components/api_tester.js.jsx
+++ app/assets/javascripts/pidgin/components/api_tester.js.jsx
@@ -5,30 +5,67 @@ class ApiTester extends React.Component {
     this.state = {};
     this.state.schema = this.props.schema;
     this.state.response = {};
+    this.state.token = '';
+    this.state.identity = {};
+    this.props.path.split('/').forEach((dir) => {
+      if(dir.match(/:/)) {
+        this.state.identity[dir] = '';
+      }
+    });
   }
 
   get endpoint() {
-    if(this.props.href.match(/{/)) {
-      return (<span>TODO</span>);
-    } else {
-      return (<span>{this.props.href}</span>);
-    }
+    return this.props.path.split('/').map((dir) => {
+      if(dir.match(/:/)) {
+        return ['/', <input onChange={this.onIndentityChange.bind(this, dir)} type='text' name={dir} />];
+      } else {
+        return dir.length > 0 ? `/${dir}` : '';
+      }
+    });
+  }
+
+  onIndentityChange(dir, e) {
+    this.setState({identity: Object.assign(this.state.identity, {[dir]: e.target.value})});
+  }
+
+  onTokenChange(e) {
+    this.setState({token: e.target.value});
   }
 
   render() {
     let schema = this.state.schema;
     return (
       <div className='api-tester'>
+        <div>oauth_token: <input onChange={this.onTokenChange.bind(this)} type='text' value={this.state.token} /></div>
         <div>{this.endpoint}</div>
         <Form
           onSubmit={this.sendRequest.bind(this)}
           schema={schema ? schema : {}}
           />
+        <div className='response'>
+          <pre>{this.state.response}</pre>
+        </div>
       </div>
     );
   }
 
-  sendRequest(formData) {
-    console.log(formData);
+  sendRequest(form) {
+    const metas = document.getElementsByTagName('meta');
+    fetch(window.location.pathname, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': metas['csrf-token']['content'],
+      },
+      body: JSON.stringify({
+        token: this.state.token,
+        identity: this.state.identity,
+        payload: form.formData
+      })
+    }).then((response) => {
+      return resposne.json();
+    }).then((json) => {
+      this.setState({response: json});
+    });
   }
 }
